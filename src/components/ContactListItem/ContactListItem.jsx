8a62369035187354e309676f85899f405505a0c3
Judielyn Pagente <git-commit-address>
2024-04-23 01:44:38 +0800

    Update 2

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,6 +1,7 @@
     import React, { Component } from 'react';
     import css from './ContactListItem.module.css';
     import PropTypes from 'prop-types';
+    import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
     export class ContactListItem extends Component {
